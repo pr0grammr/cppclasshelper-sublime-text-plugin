723e0a4a715ac721a8e7bcfff527f163bd617a41
fabianschilf94 <f.schilf94@gmail.com>
2018-12-07 13:03:38 +0100

    create menus and settings

---
+++ cppclasshelper.py
@@ -0,0 +1,8 @@
+import sublime, sublime_plugin
+
+class CreateCppClassCommand(sublime_plugin.WindowCommand):
+	'''
+	class for create a C++ class with a sourcefile and headerfile
+	'''
+	def run(self):
+		pass
